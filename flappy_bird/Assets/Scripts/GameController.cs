6d00bf22e092f4e3629bf4a265036b6e5e2d0b5d
ToanDL1 <toanlevan0123@gmail.com>
2023-10-28 09:59:41 +0700

    Up create pipe

--- flappy_bird/Assets/Scripts/GameController.cs
+++ flappy_bird/Assets/Scripts/GameController.cs
@@ -5,16 +5,35 @@ using UnityEngine;
 public class GameController : MonoBehaviour
 {
 
-    private GameObject PipePrefabs;
+    public GameObject PipePrefabs;
+
+    double TimeCreate;
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-
+        TimeCreate = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        TimeCreate -= Time.deltaTime;
+        if (TimeCreate <= 0)
+        {
+            CreatePipe();
+            TimeCreate = 2;
+        }
+    }
+    public void CreatePipe()
+    {
+        Vector2 PipePos = new Vector2(11, Random.Range((float)-4.5, (float)-2.75));
+        if (PipePrefabs)
+        {
+            Instantiate(PipePrefabs, PipePos, Quaternion.identity);
+        }
     }
 }
